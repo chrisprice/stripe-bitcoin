272ac29ba603cbe792470bb2a4da92440ab63311
Chris Price <price.c@gmail.com>
2016-11-28 12:24:44 +0000

    feat: playing with apple pay

--- index.js
+++ index.js
@@ -30,6 +30,56 @@ wallet({ bws, mnemonic })
       data-bitcoin="true">
     </script>
   </form>
+  <form id="apple-pay-form" action="" method="POST">
+    <input type="hidden" name="stripeToken"/>
+    <style>
+      #apple-pay-button {
+        display: none;
+        background-color: black;
+        background-image: -webkit-named-image(apple-pay-logo-white);
+        background-size: 100% 100%;
+        background-origin: content-box;
+        background-repeat: no-repeat;
+        width: 100%;
+        height: 44px;
+        padding: 10px 0;
+        border-radius: 10px;
+      }
+    </style>
+    <button id="apple-pay-button"></button>
+    <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+    <script>
+      Stripe.setPublishableKey('${publishableKey}');
+      Stripe.applePay.checkAvailability(function(available) {
+        if (available) {
+          document.getElementById('apple-pay-button').style.display = 'block';
+        }
+      });
+      function beginApplePay() {
+        var paymentRequest = {
+          countryCode: 'UK',
+          currencyCode: 'GBP',
+          total: {
+            label: 'topup.scottcoin.chrisprice.io',
+            amount: '${(amount / 100).toFixed(2)}'
+          }
+        };
+        var session = Stripe.applePay.buildSession(paymentRequest,
+          function(result, completion) {
+            completion(ApplePaySession.STATUS_SUCCESS);
+            var form = document.getElementById('apple-pay-form');
+            form.elements.stripeToken.value = result.token.id;
+            form.submit();
+          },
+          function(error) {
+            console.log(error.message);
+          }
+        );
+        session.begin();
+      }
+      document.getElementById('apple-pay-button').addEventListener('click', beginApplePay);
+    </script>
+  </form>
 </body>
 </html>`);
     });
